e579625fbd7b65b8199e841729194c09aff9939d
unknown <viiktoor@gmail.com>
2020-07-06 16:57:49 -0400

    splash activity

---
+++ app/src/main/java/com/alura/notesapp/ui/activity/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.alura.notesapp.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.alura.notesapp.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                goesToLogin();
+            }
+        },3000);
+    }
+
+    private void goesToLogin() {
+        Intent intent = new Intent(this, NotesListActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
